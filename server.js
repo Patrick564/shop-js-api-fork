60eaf045d0592c6d7794c32bcca656862f3ad49d
MariaF <mariafs2407@gmail.com>
2023-03-14 21:15:04 -0500

    agregando pool

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const mysql = require('mysql2')
 
 const pool = require('./src/db/connection.js')
 
-const conn = mysql.createConnection({
-  host: 'containers-us-west-187.railway.app',
-  user: 'root',
-  port: 6318,
-  password: 'WXmV1nUCekSbDetn5L4P',
-  database: 'railway'
-})
-
 const app = express()
 
 app.get('/', (req, res) => {
@@ -23,13 +15,13 @@ app.get('/productos/:id', (req, res) => {
 })
 
 app.get('/lugares', async (req, res) => {
-  conn.query('SELECT * FROM lugares', (err, results, fields) => {
+  pool.query('SELECT * FROM lugares', (err, results, fields) => {
     res.json({ 'lugares': results })
   })
 })
 
 app.get('/destinos', async (req, res) => {
-  conn.query('SELECT * FROM destino', (err, results, fields) => {
+  pool.query('SELECT * FROM destino', (err, results, fields) => {
     if (err) console.log(err)
 
     res.json({ 'res': results })
@@ -37,7 +29,7 @@ app.get('/destinos', async (req, res) => {
 })
 
 app.get('/categoria-dos', (req, res) => {
-  conn.query('SELECT * FROM categoriados', (err, results, fields) => {
+  pool.query('SELECT * FROM categoriados', (err, results, fields) => {
     if (err) console.log(err)
 
     res.json({ 'categoriados': results })
@@ -45,7 +37,7 @@ app.get('/categoria-dos', (req, res) => {
 })
 
 app.get('/categoria', (req, res) => {
-  conn.query('SELECT * FROM categoria', (err, results, fields) => {
+  pool.query('SELECT * FROM categoria', (err, results, fields) => {
     if (err) console.log(err)
 
     res.json({ 'categoria': results })
